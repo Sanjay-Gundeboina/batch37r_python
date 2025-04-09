6397032ca10919181c9b8ef529483200b5ee2f3b
Gundeboina Sanjay <sanjaygundeboina9@gmail.com>
2025-04-09 21:58:45 +0530

    swapping sorting task

--- swappingsorting.py
+++ swappingsorting.py
@@ -1,15 +1,15 @@
 '''program to separate odd & even elements from list '''
 
-# l=[1,2,6,5,3,4,8,9,7]
-# even_list=[]
-# odd_list=[]
-# for i in l:
-#     if i%2==0:
-#         even_list.append(i)
-#     else:
-#         odd_list.append(i)
-# print(even_list)
-# print(odd_list)
+l=[1,2,6,5,3,4,8,9,7]
+even_list=[]
+odd_list=[]
+for i in l:
+    if i%2==0:
+        even_list.append(i)
+    else:
+        odd_list.append(i)
+print(even_list)
+print(odd_list)
 
 '''O/P:[2, 6, 4, 8]
        [1, 5, 3, 9, 7]'''
@@ -17,14 +17,14 @@
 
 '''program to separate unique elements from list and add "*" at EOL'''
 
-# l=[1,2,3,4,3,2,6,7,8,6,9,5]
-# unique=[]
-# for i in l:
-#     if i not in unique:
-#         unique.append(i)
-#     else:
-#         unique.append("*")
-# print(unique)
+l=[1,2,3,4,3,2,6,7,8,6,9,5]
+unique=[]
+for i in l:
+    if i not in unique:
+        unique.append(i)
+    else:
+        unique.append("*")
+print(unique)
 
 
 '''O/P:[1, 2, 3, 4, '*', '*', 6, 7, 8, '*', 9, 5]'''
